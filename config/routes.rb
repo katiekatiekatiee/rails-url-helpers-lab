d324071bd6ad20f028a052d63c4b2d3e68d39f3e
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-06-29 22:37:33 -0400

    commit tree d84c6a5d19c62ce3501caed956b539d5d81aa66e

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "/students", to: "students#index", as: "students"
   get "/students/:id", to: "students#show", as: "student"
 
-  get "/students/:id/activate", to: "student#activate", as: "activate_student"
+  get "/students/:id/activate", to: "students#activate", as: "activate_student"
 end
